dfd15321a57ac05e7f7a9a16f38e0320b1bcb71c
R@y <rmarrerof87@gmail.com>
2024-06-30 10:34:14 -0400

    feat: use skill-card component

--- src/app/home/components/home-skills/home-skills.component.ts
+++ src/app/home/components/home-skills/home-skills.component.ts
@@ -1,16 +1,21 @@
-import { AfterViewInit, Component, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, signal } from '@angular/core';
 import Swiper from 'swiper';
+import { SkillCardComponent } from '../../../shared/components/skill-card/skill-card.component';
+import { DataService } from '../../../shared/services/data.service';
 
 @Component({
   selector: 'app-home-skills',
   standalone: true,
-  imports: [],
   templateUrl: './home-skills.component.html',
   styleUrl: './home-skills.component.css',
+  imports: [SkillCardComponent],
 })
 export class HomeSkillsComponent implements AfterViewInit {
   realIndex = signal<number>(1);
   slidesLength = signal<number>(1);
+  dataSrv = inject(DataService);
+
+  skills = this.dataSrv.skills;
 
   ngAfterViewInit(): void {
     new Swiper('.skills-slider .swiper', {
