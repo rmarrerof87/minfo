28e1b8796460d3148ce1896001a25e1c71338993
R@y <rmarrerof87@gmail.com>
2024-07-01 20:01:03 -0400

    build: first build

--- src/app/shared/models/testimonial.ts
+++
@@ -1,8 +0,0 @@
-export interface Testimonial {
-  id: number;
-  rate: number;
-  imageUrl: string;
-  text: string;
-  userName: string;
-  userJob: string;
-}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { register as registerSwiperElements } from 'swiper/element-bundle';
-
-registerSwiperElements();
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
